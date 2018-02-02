d938bce23c372d6a2b921e6d4282c6a2d80fbb57
belzebalex <belzebalex@users.noreply.github.com>
2018-02-02 18:51:52 +0100

    indent fix

--- src/gamelevel.cpp
+++ src/gamelevel.cpp
@@ -7,7 +7,7 @@ GameLevel::GameLevel( )
 
 void GameLevel::draw( sf::RenderTarget& target, sf::RenderStates states ) const
 {
-  target.draw( dolphin );
+  	target.draw( dolphin );
 	target.draw( intelligent_fish );
 }
 
@@ -17,6 +17,6 @@ void GameLevel::update( float linear_input, float angular_input )
 	dolphin.update( linear_input, angular_input );  	
 	std::vector<Fish> fishs;
 	fishs.push_back(dolphin);
-  intelligent_fish.giveSawFishes(&fishs);
+  	intelligent_fish.giveSawFishes(&fishs);
 	intelligent_fish.update(0, 0);
 }
